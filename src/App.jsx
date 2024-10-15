e64e5a0c99f30b22b0423cc1d87eb565866afa64
Vinicius Yudi <vinizzly2005@gmail.com>
2024-10-15 19:04:17 -0300

    add console error para API

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css';
 import TempoInfo from './components/TempoInfo/TempoInfo';
 import TempoInfo5Dias from './components/tempoInfo5Dias/TempoInfo5Dias';
 
-
 function App() {
   const [tempo, setTempo] = useState();
   const [tempo5Dias, setTempo5Dias] = useState();
+  const [erro, setErro] = useState(null);  // Para exibir erros, caso ocorra
 
   const inputRef = useRef();
 
@@ -18,11 +18,19 @@ function App() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&lang=pt_br&units=metric`;
     const url5Dias = `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${chave}&lang=pt_br&units=metric`;
 
-    const apiInfo = await axios.get(url);
-    const apiInfo5Dias = await axios.get(url5Dias);
+    try {
+      
+      const apiInfo = await axios.get(url);
+      const apiInfo5Dias = await axios.get(url5Dias);
 
-    setTempo5Dias(apiInfo5Dias.data);
-    setTempo(apiInfo.data);
+      setTempo5Dias(apiInfo5Dias.data);
+      setTempo(apiInfo.data);
+      setErro(null); 
+    } catch (error) {
+
+      console.error("Erro ao buscar dados da API:", error);
+      setErro("Erro ao buscar dados da previsão do tempo. Verifique a cidade ou tente novamente mais tarde.");
+    }
   }
 
   return (
@@ -31,6 +39,7 @@ function App() {
       <input ref={inputRef} type='text' placeholder='Digite o nome da cidade' />
       <button onClick={buscarCidade}>Buscar</button>
 
+      {erro && <p className='error'>{erro}</p>}
       {tempo && <TempoInfo tempo={tempo} />}
       {tempo5Dias && <TempoInfo5Dias tempo5Dias={tempo5Dias} />} 
     </div>
